f7bef2541936be6bbfc33bbb58cb4112831fb66c
blossom-babs <saphashb@gmail.com>
2024-05-15 11:10:20 +0100

    update connection string

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,12 +4,9 @@ import { config } from 'dotenv';
 config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT!, 10),
+  connectionString: process.env.DATABASE_URL
 });
+
 
 export default pool;
 
